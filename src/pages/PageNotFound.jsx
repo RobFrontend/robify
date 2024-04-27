adb8e87c93a2f7927ee8296f5daa96902ae27d28
RobFrontend <robert.grabowski97@gmail.com>
2024-04-27 02:01:43 +0200

    not found bug

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const NotFound = styled.div`
+  background-color: var(--body-background-color);
+  height: 100vh;
+`;
+
 function PageNotFound() {
-  return <h1>Page not Found</h1>;
+  return (
+    <NotFound>
+      <h1>Page not Found</h1>
+    </NotFound>
+  );
 }
 
 export default PageNotFound;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,8 +1,8 @@
+import { Outlet } from "react-router-dom";
 import Player from "./Player";
 import Main from "./Main";
 import CurrentPage from "./CurrentPage";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
